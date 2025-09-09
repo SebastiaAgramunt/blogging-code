9f0a0d9672d739eed81e2ff0ef697b4b77d6edd1
Sebastia Agramunt Puig <sebas@SebasComputer.local>
2025-09-08 18:59:18 -0700

    removing cuda-blur to put in PR

--- cuda-blur/include/blur_image.cuh
+++
@@ -1,7 +0,0 @@
-#ifndef BLUR_IMAGE_CUH
-#define BLUR_IMAGE_CUH
-
-__global__ void blur_kernel(unsigned char* input, unsigned char *output, int width, int height);
-void img_to_blur(unsigned char* input, int width, int height);
-
-#endif
--- cuda-blur/scripts/download-img.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-THIS_DIR=$(dirname "$(realpath "$0")")
-ROOT_DIR=$(dirname ${THIS_DIR})
-
-mkdir ${ROOT_DIR}/img
-wget "https://upload.wikimedia.org/wikipedia/commons/2/28/20100723_Miyajima_4904.jpg" -O ${ROOT_DIR}/img/raw_img.jpeg
